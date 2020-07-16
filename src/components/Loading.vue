d08f8fa00c6d449ad888fb9da12edf9dcf9f1eec
Marius K <marius.into@gmail.com>
2020-07-16 12:10:48 +0300

    Replace loading img

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,21 +1,50 @@
 <template>
     <div class="loading">
-        <img src="loading.gif" weblink="https://upload.wikimedia.org/wikipedia/commons/6/66/Loadingsome.gif"/>
-    </div>
+		<div class="loading-circle"></div>
+		<div class="loading-text">Loading</div>
+	</div>
 </template>
 
 <style>
 .loading {
-    position:absolute;
-    height:200px;
-    width:200px;
-    top:0;bottom:0;left:0;right:0;
-    margin:auto;
-    padding-left:250px;
-    z-index:9999999999;
+	width: 120px;
+	height: 120px;
+	left:0;right:0;
+	top:0;bottom:0;
+	margin:auto;
+	position:absolute;
+ }
+
+ .loading-circle {
+	position:absolute;
+	border-radius: 50%;
+	border-top: 1px solid black;
+	width: 120px;
+	height: 120px;
+	-webkit-animation: spin 2s linear infinite; /* Safari */
+	animation: spin 1s linear infinite;
+  }
+
+  .loading-text {
+    position: absolute;
+    padding-top: 52px;
+    text-align: center;
+    font-family: roboto, sans-serif, "Helvetica Neue", "Lucida Grande", Arial;
+    color: #303030;
+    width: 120px;
+    font-size: 11pt;
+	text-transform:uppercase;
 }
 
-.loading img {
-    height:100%;
+/* Safari */
+@-webkit-keyframes spin {
+	0% { -webkit-transform: rotate(0deg); }
+	100% { -webkit-transform: rotate(360deg); }
 }
+  
+@keyframes spin {
+	0% { transform: rotate(0deg); }
+	100% { transform: rotate(360deg); }
+}
+
 </style>
